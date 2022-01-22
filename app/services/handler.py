364c7395592c6d2b6489d35855ec86d86b8af5c5
Ben Browner <bnbrowner@gmail.com>
2022-01-21 23:02:58 -0500

    wut

--- app/services/handler.py
+++ app/services/handler.py
@@ -11,7 +11,7 @@ class Skip(Exception):
 class WebsocketHandler:
     ENDPOINT = "wss://ws.beatsaver.com/maps"
 
-    def init(self, client: aiohttp.ClientSession) -> None:
+    def __init__(self, client: aiohttp.ClientSession) -> None:
         self._client = client
         self.handlers = {
             "MAP_UPDATE": self.handle_map_update,
